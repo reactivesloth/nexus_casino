c4bdcec4ffc43fc7ef917f866e76400edc629b99
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2025-06-12 00:46:24 +0300

    feat: update project

---
+++ Assets/Code/Tests/UVScroller.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class UVScroller : MonoBehaviour
+{
+    [SerializeField] private Vector2 scrollSpeed;
+    [SerializeField] private int materialID = 0;
+
+    private float offset; 
+    private Renderer renderer;
+
+    private void Start()
+    {
+        offset = Random.Range(0f, 100f);
+        renderer = GetComponent<Renderer>();
+    }
+    
+    public void Update()
+    {
+        if (scrollSpeed != Vector2.zero)
+        {
+            renderer.materials[materialID].mainTextureOffset = ((Time.time + offset) * scrollSpeed);
+        }
+    }
+}
