3034231c79b5a60942eb7c669a97709c9c9235a2
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2025-06-30 07:14:10 +0300

    feat: refactored player controller and sit mechanics prototype

---
+++ Assets/Code/Network/PlayerSitInteraction/PlayerIK.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Code.Network.PlayerSitInteraction
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerIK : MonoBehaviour
+    {
+        public bool ikActive = true;
+        public Transform leftHandTarget, rightHandTarget;
+        public Transform leftFootTarget, rightFootTarget;
+
+        private Animator animator;
+        private SitController sitController;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            sitController = GetComponent<SitController>();
+        }
+
+        void OnAnimatorIK(int layerIndex)
+        {
+            if (animator && sitController != null && sitController.IsSitting() && ikActive)
+            {
+                if (leftHandTarget != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTarget.position);
+                    animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTarget.rotation);
+                }
+
+                if (rightHandTarget != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTarget.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTarget.rotation);
+                }
+
+                if (leftFootTarget != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
+                    animator.SetIKPosition(AvatarIKGoal.LeftFoot, leftFootTarget.position);
+                    animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootTarget.rotation);
+                }
+
+                if (rightFootTarget != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightFoot, rightFootTarget.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootTarget.rotation);
+                }
+            }
+            else
+            {
+                // сброс IK
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 0);
+            }
+        }
+    }
+}
