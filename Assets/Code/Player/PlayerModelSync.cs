49ae04bc18325960355d36202c915f9a460a6c05
Dmitry <perekrestovdi@yandex.com>
2025-07-18 16:01:54 +0300

    fix: try sync on client start but with delay

--- Assets/Code/Player/PlayerModelSync.cs
+++ Assets/Code/Player/PlayerModelSync.cs
@@ -1,9 +1,6 @@
-using System;
 using CC;
 using FishNet.Connection;
 using FishNet.Object;
-using FishNet.Object.Synchronizing;
-using FishNet.Transporting;
 using UnityEngine;
 
 namespace Code.Player
@@ -13,43 +10,39 @@ namespace Code.Player
     {
         private CharacterCustomization _characterCustomization;
 
-        private readonly SyncVar<string> _characterCustomizationJson = new(new SyncTypeSettings
-        {
-            WritePermission = WritePermission.ClientUnsynchronized,
-            ReadPermission = ReadPermission.Observers,
-            Channel = Channel.Reliable
-        });
-
         private void Awake()
         {
             _characterCustomization = GetComponent<CharacterCustomization>();
         }
 
-        private void OnEnable()
-        {
-            _characterCustomizationJson.OnChange += CharacterCustomizationJsonOnOnChange;
-        }
-
-        private void OnDisable()
-        {
-            _characterCustomizationJson.OnChange -= CharacterCustomizationJsonOnOnChange;
-        }
-
         public override void OnStartClient()
         {
             base.OnStartClient();
-            Debug.Log($"OnStartClient: {Owner}. Is owner = {IsOwner}");
-            if (IsOwner)
-                _characterCustomizationJson.Value = _characterCustomization.GetJSON();
-            //TransmitLocalCharacter();
+            Invoke(nameof(TransmitLocalCharacter), 1f);
         }
-        
-        private void CharacterCustomizationJsonOnOnChange(string prev, string next, bool asServer)
+
+        [ServerRpc]
+        public void SendCharacterJsonServerRpc(string json, NetworkConnection sender = null)
         {
+            // на сервере логируем и ретранслируем всем остальным
+            Debug.Log($"[Server] Получен JSON ({json.Length} симв.) от {sender.ClientId}");
+            SendCharacterJsonObserversRpc(json);
+        }
+
+        [ObserversRpc(BufferLast = true)]
+        private void SendCharacterJsonObserversRpc(string json)
+        {
+            Debug.Log($"[Client] Получил JSON ({json.Length} симв.)");
+            // восстанавливаем из JSON
+            _characterCustomization.LoadFromJSON(json);
+        }
+
+        public void TransmitLocalCharacter()
+        {
+            if (!IsOwner) return; 
             
-            Debug.Log($"Clent {Owner} recived:\n{next}");
-            if(!string.IsNullOrEmpty(next))
-                _characterCustomization.LoadFromJSON(next);
+            string json = _characterCustomization.GetJSON();
+            SendCharacterJsonServerRpc(json);
         }
     }
 }
