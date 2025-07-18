67d058f86e0c9a0ebe85d67e5c41c492816bfaa9
Dmitry <perekrestovdi@yandex.com>
2025-07-18 15:28:11 +0300

    feat: not sync

--- Assets/Code/Player/PlayerModelSync.cs
+++ Assets/Code/Player/PlayerModelSync.cs
@@ -1,6 +1,9 @@
+using System;
 using CC;
 using FishNet.Connection;
 using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using FishNet.Transporting;
 using UnityEngine;
 
 namespace Code.Player
@@ -10,17 +13,28 @@ namespace Code.Player
     {
         private CharacterCustomization _characterCustomization;
 
+        private readonly SyncVar<string> _characterCustomizationJson = new(new SyncTypeSettings
+        {
+            WritePermission = WritePermission.ClientUnsynchronized,
+            ReadPermission = ReadPermission.Observers,
+            Channel = Channel.Reliable
+        });
+
         private void Awake()
         {
             _characterCustomization = GetComponent<CharacterCustomization>();
         }
 
-        public override void OnStartClient()
+        private void OnEnable()
         {
-            base.OnStartClient();
-            TransmitLocalCharacter();
+            _characterCustomizationJson.OnChange += CharacterCustomizationJsonOnOnChange;
         }
 
+        private void OnDisable()
+        {
+            _characterCustomizationJson.OnChange -= CharacterCustomizationJsonOnOnChange;
+        }
+        
         [ServerRpc]
         public void SendCharacterJsonServerRpc(string json, NetworkConnection sender = null)
         {
@@ -43,6 +57,11 @@ namespace Code.Player
             
             string json = _characterCustomization.GetJSON();
             SendCharacterJsonServerRpc(json);
+        }
+        
+        private void CharacterCustomizationJsonOnOnChange(string prev, string next, bool asServer)
+        {
+            _characterCustomization.LoadFromJSON(next);
         }
     }
 }
