c19a3d7cf5a60c4c0c285804b476a95187e17d88
Dmitry <perekrestovdi@yandex.com>
2025-08-01 20:39:24 +0300

    feat: voice input from player input

---
+++ Assets/Code/Player/VoiceChatInputHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using Dissonance;
+using UnityEngine;
+
+namespace Code.Player
+{
+    public class VoiceChatInputHandler : MonoBehaviour
+    {
+        [SerializeField] private VoiceBroadcastTrigger voiceBroadcastTrigger;
+
+        private void OnValidate()
+        {
+            voiceBroadcastTrigger ??= GetComponent<VoiceBroadcastTrigger>();
+        }
+
+        private void Update()
+        {
+            voiceBroadcastTrigger.IsMuted = !PlayerInput.Instance.VoiceHeld;
+        }
+    }
+}
