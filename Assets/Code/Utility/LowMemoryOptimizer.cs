8b0e58b66ea0ccce461e181ae86d47c44b66d72c
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2025-08-22 00:06:04 +0300

    quality settings and materials

--- Assets/Code/Utility/LowMemoryOptimizer.cs
+++ Assets/Code/Utility/LowMemoryOptimizer.cs
@@ -11,8 +11,17 @@ namespace Code.Utility
         private void OnEnable()  => Application.lowMemory += ApplicationOnLowMemory;
         private void OnDisable() => Application.lowMemory -= ApplicationOnLowMemory;
 
+        [SerializeField] private float unloadResourcesInterval = 60;
+        
+        private void Awake()
+        {
+            if (unloadResourcesInterval > 0)
+                Invoke(nameof(ApplicationOnLowMemory), unloadResourcesInterval);
+        }
+
         private void ApplicationOnLowMemory()
         {
+            Debug.Log("[MEMORY] Low Memory Optimizer collect GC and unload ol unused resources.");
             GC.Collect();
 #if UNITY_EDITOR
             EditorUtility.UnloadUnusedAssetsImmediate();
