ef7eeb8d882e59cdea18c1d4d120074f3daf349b
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2025-09-05 03:17:52 +0300

    fix: unload app from background (except low ram)

--- Assets/Code/Utility/Editor/AndroidManifestModifier.cs
+++ Assets/Code/Utility/Editor/AndroidManifestModifier.cs
@@ -1,54 +1,67 @@
 #if UNITY_EDITOR
 using System.IO;
 using System.Xml;
+using UnityEngine;
 using UnityEditor.Android;
 
 public class AndroidManifestModifier : IPostGenerateGradleAndroidProject
-{
-    public int callbackOrder => 999; // вызываем в конце
+{ 
+    public int callbackOrder => 999;
 
     public void OnPostGenerateGradleAndroidProject(string path)
     {
-        // Путь к манифесту в итоговом проекте
         string manifestPath = Path.Combine(path, "src", "main", "AndroidManifest.xml");
         if (!File.Exists(manifestPath))
         {
-            UnityEngine.Debug.LogWarning("AndroidManifest.xml не найден: " + manifestPath);
+            Debug.LogWarning("AndroidManifest.xml не найден: " + manifestPath);
             return;
         }
 
         var xmlDoc = new XmlDocument();
         xmlDoc.Load(manifestPath);
 
-        // Пространство имён android
         var nsMgr = new XmlNamespaceManager(xmlDoc.NameTable);
         nsMgr.AddNamespace("android", "http://schemas.android.com/apk/res/android");
 
-        // Находим тег <application>
         var applicationNode = xmlDoc.SelectSingleNode("/manifest/application");
         if (applicationNode == null)
         {
-            UnityEngine.Debug.LogError("Не найден тег <application> в манифесте");
+            Debug.LogError("Не найден тег <application> в манифесте");
             return;
         }
 
-        // Проверяем наличие атрибута
         var allowBackupAttr = applicationNode.Attributes["android:allowBackup", nsMgr.LookupNamespace("android")];
         if (allowBackupAttr == null)
         {
-            // Если нет — добавляем
             allowBackupAttr = xmlDoc.CreateAttribute("android", "allowBackup", nsMgr.LookupNamespace("android"));
-            allowBackupAttr.Value = "false"; // или "true", смени под свой кейс
+            allowBackupAttr.Value = "false";
             applicationNode.Attributes.Append(allowBackupAttr);
         }
         else
         {
-            // Если есть — можно переопределить
             allowBackupAttr.Value = "false";
         }
 
+        var activityNodes = xmlDoc.SelectNodes("/manifest/application/activity") ?? throw new InvalidDataException("Illegal xml.");
+        for (var i = 0; i < activityNodes.Count; i++)
+        {
+            var activityNode = activityNodes[i];;
+            var nameAttribute = activityNode.Attributes!["android:name"];
+            if (nameAttribute == null) continue;
+            if (nameAttribute.Value != "com.unity3d.player.UnityPlayerGameActivity") continue;
+
+            var launchAttribute = activityNode.Attributes!["android:launchMode"];
+            if (launchAttribute != null)
+            {
+                launchAttribute.Value = "standard";
+                Debug.Log("✅ Применен android:launchMode");
+            }
+
+            break;
+        }
+
         xmlDoc.Save(manifestPath);
-        UnityEngine.Debug.Log("✅ AndroidManifest.xml успешно обновлён (allowBackup=false)");
+        Debug.Log("✅ AndroidManifest.xml успешно обновлён (allowBackup=false)");
     }
 }
 #endif
