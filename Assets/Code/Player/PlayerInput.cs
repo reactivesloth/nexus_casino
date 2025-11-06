721563d9b18079c9794a87a9c567b7d30841d324
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2025-11-06 19:25:43 +0300

    fix: voicechat swap button to toggle

--- Assets/Code/Player/PlayerInput.cs
+++ Assets/Code/Player/PlayerInput.cs
@@ -232,7 +232,7 @@ public class PlayerInput : MonoBehaviour
     }
     
     public bool JumpDown  => !IsChatOpened && !IsBusy && (IsUsingMobileFallback && JumpButton != null ? JumpButton.GetButtonDown() : _player.Jump is { triggered: true });
-    public bool VoiceHeld => !IsChatOpened && !IsBusy && (IsUsingMobileFallback && VoiceButton != null ? VoiceButton.GetButton()   : _player.Voice != null && _player.Voice.ReadValue<float>() > 0.5f);
+    public bool VoiceHeld => IsUsingMobileFallback && VoiceButton != null ? VoiceButton.GetButton() : _player.Voice is {triggered: true};
     public bool SprintHeld => !IsChatOpened && !IsBusy && (IsUsingMobileFallback && MoveJoystick != null ? Mathf.Abs(MoveJoystick.VerticalAxis) > 0.85f || Mathf.Abs(MoveJoystick.HorizontalAxis) > 0.85f : _player.Sprint != null && _player.Sprint.ReadValue<float>() > 0.5f);
     public bool CameraSwitchDown => !IsChatOpened && !IsBusy && (IsUsingMobileFallback && CameraSwitchButton != null ? CameraSwitchButton.GetButtonDown() : _player.CameraSwitch is { triggered: true });
     public bool InteractDown => !IsChatOpened && !IsBusy && (IsUsingMobileFallback && InteractButton != null ? InteractButton.GetButtonDown() : _player.Interact is { triggered: true });
--- Assets/Code/Player/VoiceChatInputHandler.cs
+++ Assets/Code/Player/VoiceChatInputHandler.cs
@@ -1,21 +1,41 @@
-using System;
 using Dissonance;
 using UnityEngine;
+using Gradient = Ricimi.Gradient;
 
 namespace Code.Player
 {
     public class VoiceChatInputHandler : MonoBehaviour
     {
         [SerializeField] private VoiceBroadcastTrigger voiceBroadcastTrigger;
+        [SerializeField] private Gradient mobileButtonImage;
+        [SerializeField] private Color on1, on2, off1, off2;
 
-        private void OnValidate()
+        private bool voiceHeld = false;
+        private float saveTime;
+        
+        private void Awake()
         {
             voiceBroadcastTrigger ??= GetComponent<VoiceBroadcastTrigger>();
         }
 
         private void Update()
         {
-            voiceBroadcastTrigger.VoiceHeld = PlayerInput.Instance.VoiceHeld;
+            if (saveTime > 0)
+                saveTime -= Time.deltaTime;
+            else if (PlayerInput.Instance.VoiceHeld)
+            {
+                saveTime = 0.2f;
+                voiceHeld = !voiceHeld;
+                voiceBroadcastTrigger.VoiceHeld = voiceHeld;
+                
+                if (mobileButtonImage != null)
+                {
+                    mobileButtonImage.Color1 = voiceHeld ? on1 : off1;
+                    mobileButtonImage.Color2 = voiceHeld ? on2 : off2;
+                    mobileButtonImage.gameObject.SetActive(false);
+                    mobileButtonImage.gameObject.SetActive(true);
+                }
+            }
         }
     }
 }
