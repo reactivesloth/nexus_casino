ae257b8889a57b7367a3c0dd17e01a4ad097a5f2
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2025-11-21 17:37:33 +0300

    fix: voicechat ask every time permission

--- Assets/Code/Player/VoiceChatInputHandler.cs
+++ Assets/Code/Player/VoiceChatInputHandler.cs
@@ -1,5 +1,8 @@
+using Code.UI.Popup;
 using Dissonance;
+using Ricimi;
 using UnityEngine;
+using UnityEngine.UI;
 using Gradient = Ricimi.Gradient;
 
 namespace Code.Player
@@ -13,9 +16,12 @@ namespace Code.Player
         private bool voiceHeld = false;
         private float saveTime;
         
+        private NexusModularPopupOpener _popupOpener;
+        
         private void Awake()
         {
             voiceBroadcastTrigger ??= GetComponent<VoiceBroadcastTrigger>();
+            _popupOpener = FindAnyObjectByType<NexusModularPopupOpener>(FindObjectsInactive.Include);
         }
 
         private void Update()
@@ -24,29 +30,60 @@ namespace Code.Player
                 saveTime -= Time.deltaTime;
             else if (PlayerInput.Instance.VoiceHeld)
             {
+                if (voiceHeld) VoiceChatHandle(false);
+                else
+                {
 #if UNITY_ANDROID
                 if (AndroidRuntimePermissions.CheckPermission("android.permission.RECORD_AUDIO"))
                 {
-                    VoiceChatHandle(!voiceHeld);
+                    VoiceChatHandle(true);
                 }
                 else
                 {
                     RequestPermission();
                 }
 #else
-                VoiceChatHandle(!voiceHeld);
+                    VoiceChatHandle(true);
 #endif
+                }
             }
         }
-
+        
 #if UNITY_ANDROID
         async void RequestPermission()
         {
             saveTime = 1000000;
             AndroidRuntimePermissions.Permission result = await AndroidRuntimePermissions.RequestPermissionAsync( "android.permission.RECORD_AUDIO" );
-            if (result == AndroidRuntimePermissions.Permission.Granted || result == AndroidRuntimePermissions.Permission.ShouldAsk)
+            if (result == AndroidRuntimePermissions.Permission.Granted)
             {
                 VoiceChatHandle(!voiceHeld);
+            } 
+            else if (result != AndroidRuntimePermissions.Permission.ShouldAsk)
+            {
+                if (_popupOpener != null)
+                {
+                    _popupOpener.Title = "Voice chat require permission for recording";
+                    _popupOpener.Subtitle = "";
+                    _popupOpener.Message = "You need give permission for recording for voice chat enable";
+            
+                    var okButton = new ButtonInfo
+                    {
+                        Label = "Allow",
+                        ClosePopupWhenClicked = true,
+                        OnClickedEvent = new Button.ButtonClickedEvent()
+                    };
+                    var cancellButton = new ButtonInfo
+                    {
+                        Label = "No",
+                        ClosePopupWhenClicked = false,
+                        OnClickedEvent = new Button.ButtonClickedEvent()
+                    };
+                    okButton.OnClickedEvent.AddListener(RequestPermission);
+                    cancellButton.OnClickedEvent.AddListener(_popupOpener.ClosePopup);
+                    _popupOpener.Buttons.Add(okButton);
+                    _popupOpener.Buttons.Add(cancellButton);
+                    _popupOpener.OpenPopup();
+                }
             }
         }
 #endif
