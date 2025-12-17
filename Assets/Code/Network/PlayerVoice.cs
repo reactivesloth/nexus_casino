a5ead78fdd91852678e1796282265228ff0b60c0
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2025-12-17 11:34:51 +0300

    fix: vuplex errors

--- Assets/Code/Network/PlayerVoice.cs
+++ Assets/Code/Network/PlayerVoice.cs
@@ -87,10 +87,17 @@ namespace Code.Network
                 {
                     if (participant != null)
                     {
-                        var audioEnergy = participant.AudioEnergy;
-                        if (participant.IsMuted) audioEnergy = 0f;
-                        if (audioEnergy < 0.01f) audioEnergy = 0f;
-                        salsa.analysisValue = (float)audioEnergy;
+                        if (participant.SpeechDetected)
+                        {
+                            var audioEnergy = participant.AudioEnergy;
+                            if (participant.IsMuted) audioEnergy = 0f;
+                            if (audioEnergy < 0.01f) audioEnergy = 0f;
+                            salsa.analysisValue = (float)audioEnergy;
+                        }
+                        else
+                        {
+                            salsa.analysisValue = 0;
+                        }
                     }
                 }
 
