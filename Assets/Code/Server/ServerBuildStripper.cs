12d882f3f4d105cbb14817389ccab6870bf4b0f3
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2026-01-13 23:42:48 +0300

    feat: server stripper and main optimizations

---
+++ Assets/Code/Server/ServerBuildStripper.cs
@@ -0,0 +1,52 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Code.Server
+{
+    public class ServerBuildProcessor : IProcessSceneWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnProcessScene(UnityEngine.SceneManagement.Scene scene, BuildReport report)
+        {
+            if (!BuildPipeline.isBuildingPlayer) return;
+
+            if (report.summary.platform != BuildTarget.StandaloneLinux64) 
+            {
+                return;
+            }
+
+            Debug.Log($"[ServerStripping] Обработка сцены: {scene.name}...");
+
+            var manifests = Object.FindObjectsByType<ServerStrippingList>(FindObjectsSortMode.None);
+
+            if (manifests.Length == 0) return;
+
+            int totalRemoved = 0;
+
+            foreach (var manifest in manifests)
+            {
+                foreach (var obj in manifest.ObjectsToStrip)
+                {
+                    if (obj != null)
+                    {
+                        Debug.Log($"[ServerStripping] Удаление: {obj.name}");
+                        Object.DestroyImmediate(obj);
+                        totalRemoved++;
+                    }
+                }
+
+                if (manifest.DestroySelfOnServer)
+                {
+                    Object.DestroyImmediate(manifest.gameObject);
+                }
+            }
+
+            Debug.Log($"[ServerStripping] Итого удалено корневых объектов: {totalRemoved}.");
+        }
+    }
+}
+#endif
---
+++ Assets/Code/Server/ServerStrippingList.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Code.Server
+{
+    public class ServerStrippingList : MonoBehaviour
+    {
+        public bool DestroySelfOnServer = true;
+        public GameObject[] ObjectsToStrip;
+    }
+}
