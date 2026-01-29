69eef1cea72e432d3ba2de42bd4e15ce94371001
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2026-01-29 20:12:01 +0300

    fix: purrnet metavc voice

--- Assets/Third Party/Metater/MetaVoiceChat/NetProviders/PurrNet/PurrNetNetProvider.cs
+++ Assets/Third Party/Metater/MetaVoiceChat/NetProviders/PurrNet/PurrNetNetProvider.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using PurrNet;
 
 namespace MetaVoiceChat.NetProviders.PurrNet
@@ -7,38 +6,28 @@ namespace MetaVoiceChat.NetProviders.PurrNet
     [RequireComponent(typeof(MetaVc))]
     public class PurrNetNetProvider : NetworkBehaviour, INetProvider
     {
-        public static PurrNetNetProvider LocalPlayerInstance { get; private set; }
-        private static readonly List<PurrNetNetProvider> s_Instances = new();
-        public static IReadOnlyList<PurrNetNetProvider> Instances => s_Instances;
+        private MetaVc MetaVc { get; set; }
 
-        public MetaVc MetaVc { get; private set; }
-
-        private const int DefaultMaxDataBytesPerPacket = 1200;
+        private const int DefaultMaxDataBytesPerPacket = 1000;
 
         protected override void OnSpawned()
         {
-            if (isOwner) LocalPlayerInstance = this;
-            s_Instances.Add(this);
+            base.OnSpawned();
+            Invoke("Init", 2);
+        }
 
+        private void Init()
+        {
             MetaVc = GetComponent<MetaVc>();
             MetaVc.StartClient(this, isOwner, DefaultMaxDataBytesPerPacket);
         }
-
+        
         protected override void OnDespawned()
         {
-            if (isOwner) LocalPlayerInstance = null;
-            s_Instances.Remove(this);
             MetaVc.StopClient();
         }
 
-        public bool IsLocalPlayerDeafened
-        {
-            get
-            {
-                if (LocalPlayerInstance == null) return false;
-                return LocalPlayerInstance.MetaVc.isDeafened;
-            }
-        }
+        public bool IsLocalPlayerDeafened => false;
 
         public void RelayFrame(int index, double timestamp, System.ReadOnlySpan<byte> data)
         {
