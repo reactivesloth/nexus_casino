5c5fc04c4d4f1a0988accf3614651aaa4ac9ce12
Nikita Kolpakov <n.kolpakov@reactivesloth.com>
2026-02-05 04:17:52 +0300

    fix: tooltip interaction

--- Assets/Code/Network/InteractionSystem/PlayerInteraction.cs
+++ Assets/Code/Network/InteractionSystem/PlayerInteraction.cs
@@ -168,6 +168,7 @@ namespace Code.Network.InteractionSystem
             if (Active != null)
             {
                 //InteractionUIHint.Instance.ShowPrompt(endInteractText);
+                InteractionUIHint.Instance.HidePrompt();
                 InteractionUIHint.Instance.HideSocial();
             }
             else if (_hovered != null)
